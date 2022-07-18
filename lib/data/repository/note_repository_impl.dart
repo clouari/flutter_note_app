83160ce48dcb59fc470eb182568c894aaa51a3a9
clouari <ekdudekgml35@naver.com>
2022-07-18 17:41:21 +0900

    4. (3번: domain>repo)에서 정의한 기능들을 data layer 안에서 실제 구현할 예정 implements로 가져오고 모두 override하기

---
+++ lib/data/repository/note_repository_impl.dart
@@ -0,0 +1,34 @@
+import 'package:flutter_note_app/domain/model/note.dart';
+import 'package:flutter_note_app/domain/repository/note_repository.dart';
+
+class NoteRepositoryImpl implements NoteRepository {
+  @override
+  Future<void> deleteNote(Note note) {
+    // TODO: implement deleteNote
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<Note> getNoteById(int it) {
+    // TODO: implement getNoteById
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<List<Note>> getNotes() {
+    // TODO: implement getNotes
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<void> insertNote(Note note) {
+    // TODO: implement insertNote
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<void> updateNote(Note note) {
+    // TODO: implement updateNote
+    throw UnimplementedError();
+  }
+}
