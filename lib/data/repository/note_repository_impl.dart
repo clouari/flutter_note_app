02d1c11f0815fddbbc2bff2e7c14666688eca811
clouari <ekdudekgml35@naver.com>
2022-07-19 13:47:36 +0900

    9. Note 기능 구현하는 구현체는 완료됨.

--- lib/data/repository/note_repository_impl.dart
+++ lib/data/repository/note_repository_impl.dart
@@ -5,36 +5,33 @@ import 'package:flutter_note_app/domain/repository/note_repository.dart';
 class NoteRepositoryImpl implements NoteRepository {
   final NoteDb db;
   // generate (comm+N) 생성자 만들어주기
+  // return이 없을 때는 await , return 이면서 결과를 받아서 써주기 위해서는 return await 같이 써주면 된다.
 
   NoteRepositoryImpl(this.db);
 
   @override
-  Future<void> deleteNote(Note note) {
-    // TODO: implement deleteNote
-    throw UnimplementedError();
+  Future<void> deleteNote(Note note) async {
+    await db.deleteNote(note);
+  }
+
+  /* nullable 처리 하면서 note_repo_impl 에서도 같이 nullable로 변경함 */
+  @override
+  Future<Note?> getNoteById(int id) async {
+    return await db.getNoteById(id);
   }
 
   @override
-  Future<Note> getNoteById(int it) {
-    // TODO: implement getNoteById
-    throw UnimplementedError();
+  Future<List<Note>> getNotes() async {
+    return await db.getNotes();
   }
 
   @override
-  Future<List<Note>> getNotes() {
-    // TODO: implement getNotes
-    throw UnimplementedError();
+  Future<void> insertNote(Note note) async {
+    await db.insertNote(note);
   }
 
   @override
-  Future<void> insertNote(Note note) {
-    // TODO: implement insertNote
-    throw UnimplementedError();
-  }
-
-  @override
-  Future<void> updateNote(Note note) {
-    // TODO: implement updateNote
-    throw UnimplementedError();
+  Future<void> updateNote(Note note) async {
+    await db.updateNote(note);
   }
 }
