3eaee383b7739f7d4e79f64d45729b7997088a0e
clouari <ekdudekgml35@naver.com>
2022-07-19 14:37:42 +0900

    14. note 삭제하는 delete_note use_case 작성

---
+++ lib/domain/use_case/delete_note.dart
@@ -0,0 +1,13 @@
+import 'package:flutter_note_app/domain/model/note.dart';
+import 'package:flutter_note_app/domain/repository/note_repository.dart';
+
+class DeleteNote {
+  // note를 디루려면 NoteRepository가 필요함
+  final NoteRepository repository;
+
+  DeleteNote(this.repository);
+
+  Future<void> call(Note note) async {
+    await repository.deleteNote(note);
+  }
+}
