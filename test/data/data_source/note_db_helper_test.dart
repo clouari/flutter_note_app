f7cd00948345bc68fcb1b57aec01cae844071d81
clouari <ekdudekgml35@naver.com>
2022-07-19 14:17:08 +0900

    12. databaseFactoryFfi 이용해서 delete, update등 모두 작동되는지 test하기 이런식으로 작성하면 됨.

--- test/data/data_source/note_db_helper_test.dart
+++ test/data/data_source/note_db_helper_test.dart
@@ -22,6 +22,23 @@ void main() {
 
       expect((await noteDbHelper.getNotes()).length, 1);
 
+      /* test2  */
+      Note note = (await noteDbHelper.getNoteById(1))!;
+      // null이 아닌 것을 이미 알고 있으니까 뒤에 ! 를 붙여서 nullable이 아님을 보증하기
+
+      expect(note.id, 1);
+
+      /* update test -> 불변객체 freezed에서 제공하는 coywith로 안에서 변경 해 보기
+      * 바뀌는지 테스트 해 보고 통과되면 delete로 진행 */
+      await noteDbHelper.updateNote(note.copyWith(title: 'change test'));
+
+      note = (await noteDbHelper.getNoteById(1))!;
+      expect(note.title, 'change test');
+
+      /* Delete test */
+      await noteDbHelper.deleteNote(note);
+      expect((await noteDbHelper.getNotes()).length, 0);
+
       // db 종료를 해 주어야 test가 오래 걸리지 않음
       await db.close();
     },
