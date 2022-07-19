678b3e0992d8dba82b336378118d6626ce14ae1c
clouari <ekdudekgml35@naver.com>
2022-07-19 15:35:44 +0900

    20. viewmodel에서 할 수 있는 일들을 먼저 모두 작성해 놓기

--- lib/presentation/notes/notes_view_model.dart
+++ lib/presentation/notes/notes_view_model.dart
@@ -1,8 +1,60 @@
 import 'package:flutter/cupertino.dart';
+import 'package:flutter_note_app/domain/model/note.dart';
 import 'package:flutter_note_app/domain/repository/note_repository.dart';
+import 'package:flutter_note_app/presentation/notes/notes_event.dart';
+import 'package:freezed_annotation/freezed_annotation.dart';
 
 class NotesViewModel with ChangeNotifier {
   final NoteRepository repository;
 
+  List<Note> _notes = [];
+  UnmodifiableListView<Note> get notes => UnmodifiableListView(_notes);
+  // private 으로 외부에서 접근 불가능하게 선언했으면, nmodifiableListView , getter로 외부에 노출하기
+
+  Note? _recentlyDeletedNote; // 마지막에 삭제된 노트는 여기에 저장
+
   NotesViewModel(this.repository);
+
+  /* 인간이 놓칠 수 있는 것들을 놓치지 않기 위해 만드는 것
+  * -> 이 화면에서 발생하는 이벤트들은 onEvent라는 하나의 메서드 통해서만 실행할 것임.
+   */
+  void onEvent(NotesEvent event) {
+    event.when(
+      loadNotes: _loadNotes,
+      deleteNote: _deleteNote,
+      restoreNote: _restoreNote,
+    );
+  }
+
+  /* loadNotes */
+  Future<void> _loadNotes() async {
+    List<Note> notes = await repository.getNotes();
+    // 갖고오면 이 데이터를 저장할 부분이 필요하기 때문에 10번줄에서 공간 만들어 주기
+    _notes = notes;
+    notifyListeners(); // 이후에 provider 사용할 것이기 때문에 상태가 바뀔 땐 notifyListeners 선언해주기
+  }
+
+  /* DeleteNotes */
+  Future<void> _deleteNote(Note note) async {
+    await repository.deleteNote(note);
+
+    _recentlyDeletedNote = note; // 마지막에 delete된 노트는 여기에 저장을 함
+
+    await _loadNotes(); // 삭제를 해 준 다음에는 데이터를 다시 읽어올 것
+    notifyListeners(); // 이후에 provider 사용할 것이기 때문에 상태가 바뀔 땐 notifyListeners 선언해주기
+  }
+
+  /* restoreNote */
+  Future<void> _restoreNote() async {
+    /* 만약 삭제된 노트가 null이 아니라면(있다면) 삭제된 정보를 가지고, insert를 해 줌
+     이후에 _recentlyDeletedNote를 다시 비워줌
+     restore가 되면 다시 load를 하자! */
+
+    if (_recentlyDeletedNote != null) {
+      repository.insertNote(_recentlyDeletedNote!);
+      _recentlyDeletedNote = null;
+
+      _loadNotes();
+    }
+  }
 }
