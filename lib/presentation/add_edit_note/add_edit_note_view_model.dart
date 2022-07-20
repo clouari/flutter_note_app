73ac39c09059d8c835f456094d18a2850cb60240
clouari <ekdudekgml35@naver.com>
2022-07-20 13:34:13 +0900

    23. 노트 작성할 때, 노트 색상 변경할 때 변경되는 상태 값 모두 view_model에서 선언 -> 내용이 입력 안됐을 때 처리부분은 view/view_model 중 어디서 할지 생각해보기

---
+++ lib/presentation/add_edit_note/add_edit_note_view_model.dart
@@ -0,0 +1,47 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_note_app/domain/model/note.dart';
+import 'package:flutter_note_app/domain/repository/note_repository.dart';
+import 'package:flutter_note_app/presentation/add_edit_note/add_edit_note_event.dart';
+
+class AddEditNoteViewModel with ChangeNotifier {
+  final NoteRepository repository;
+
+  int _color =
+      Colors.orange.value; // private color rkqtdl int 이기 때문에 아래처럼 값 변경하기
+  int get color => _color;
+
+  AddEditNoteViewModel(this.repository);
+
+  void onEvent(AddEditNoteEvent event) {
+    event.when(changeColor: _changeColor, saveNote: _saveNote);
+  }
+
+  // 아래부분 만들어 놓고, 위에서 연결
+  Future<void> _changeColor(int color) async {
+    _color = color;
+    notifyListeners();
+  }
+
+  Future<void> _saveNote(int? id, String title, String content) async {
+    if (id == null) {
+      await repository.insertNote(
+        Note(
+          title: title,
+          content: content,
+          timestamp: DateTime.now().millisecondsSinceEpoch,
+          color: _color,
+        ),
+      );
+    } else {
+      await repository.updateNote(
+        Note(
+            id: id,
+            title: title,
+            content: content,
+            timestamp: DateTime.now().millisecondsSinceEpoch,
+            color: _color),
+      );
+    }
+  }
+}
