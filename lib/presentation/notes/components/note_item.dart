369a0ed9c26ddc466121aba21b77f6bff1b30ebb
clouari <ekdudekgml35@naver.com>
2022-07-20 15:16:58 +0900

    26.[ UI ] main UI(아직 클릭시 작동되는 콜백함수는 없는 상태) 1차 완성

---
+++ lib/presentation/notes/components/note_item.dart
@@ -0,0 +1,53 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_note_app/domain/model/note.dart';
+import 'package:flutter_note_app/ui/colors.dart';
+
+class NoteItem extends StatelessWidget {
+  final Note note;
+
+  const NoteItem({Key? key, required this.note}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: Stack(
+        children: [
+          Container(
+            width: double.infinity,
+            padding: const EdgeInsets.all(8),
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(10),
+              color: Color(note.color),
+            ),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  note.title,
+                  maxLines: 1, // 최대 한 줄까지만 보여주기
+                  overflow: TextOverflow.ellipsis, // 넘어가는 것은 ... 으로!
+                  style: Theme.of(context)
+                      .textTheme
+                      .headline6, // 하나하나 상세지정하지 말고 동일하게 theme으로!
+                ),
+                const SizedBox(height: 8),
+                Text(
+                  note.content,
+                  maxLines: 3,
+                  overflow: TextOverflow.ellipsis,
+                  style: Theme.of(context).textTheme.bodyText2,
+                ),
+              ],
+            ),
+          ),
+          Positioned(
+            bottom: 8,
+            right: 8,
+            child: Icon(Icons.delete, color: darkGray),
+          ),
+        ],
+      ),
+    );
+  }
+}
--- lib/presentation/notes/notes_screen.dart
+++ lib/presentation/notes/notes_screen.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_note_app/domain/model/note.dart';
+import 'package:flutter_note_app/presentation/notes/components/note_item.dart';
+import 'package:flutter_note_app/ui/colors.dart';
 
 class NotesScreen extends StatelessWidget {
   const NotesScreen({Key? key}) : super(key: key);
@@ -20,10 +23,32 @@ class NotesScreen extends StatelessWidget {
           )
         ],
       ),
-      body: Container(),
       floatingActionButton: FloatingActionButton(
         onPressed: () {},
         child: const Icon(Icons.add),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: ListView(
+          children: [
+            NoteItem(
+              note: Note(
+                title: 'title 1',
+                content: 'content 1',
+                color: wisteria.value, // color type
+                timestamp: 1,
+              ),
+            ),
+            NoteItem(
+              note: Note(
+                title: 'title 2',
+                content: 'content 2',
+                color: skyBlue.value, // color type
+                timestamp: 2,
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
