0cdc8e759d2ba2fe361921382c899c239c4daf8e
clouari <ekdudekgml35@naver.com>
2022-07-21 13:50:36 +0900

    32.[ UI + 기능 ] 임시로 컬러값을 미리 넣어두었던 것 제거하고 이전에 color 지정해두었던 걸로 변경

--- lib/presentation/add_edit_note/add_edit_note_view_model.dart
+++ lib/presentation/add_edit_note/add_edit_note_view_model.dart
@@ -3,12 +3,13 @@ import 'package:flutter/material.dart';
 import 'package:flutter_note_app/domain/model/note.dart';
 import 'package:flutter_note_app/domain/repository/note_repository.dart';
 import 'package:flutter_note_app/presentation/add_edit_note/add_edit_note_event.dart';
+import 'package:flutter_note_app/ui/colors.dart';
 
 class AddEditNoteViewModel with ChangeNotifier {
   final NoteRepository repository;
 
-  int _color =
-      Colors.orange.value; // private color rkqtdl int 이기 때문에 아래처럼 값 변경하기
+  int _color = roseBud.value;
+  // Colors.orange.value; // private color rkqtdl int 이기 때문에 아래처럼 값 변경하기
   int get color => _color;
 
   AddEditNoteViewModel(this.repository);
