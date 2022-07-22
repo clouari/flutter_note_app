ed4b0bc4601740d1686fb72226fefea467ec35d5
clouari <ekdudekgml35@naver.com>
2022-07-22 16:30:30 +0900

    56. [ 옵션넣기 ]  하나의 타입으로 하기위해 봉인(sealed) Class 로 사용해서 기능 완료함

--- lib/domain/use_case/get_notes_use_case.dart
+++ lib/domain/use_case/get_notes_use_case.dart
@@ -9,20 +9,41 @@ class GetNotesUseCase {
   GetNotesUseCase(this.repository);
 
   /*모든 class 가 가지고 있는 call method 재정의하기*/
+  /* 하나의 타입으로 하기위해 봉인(sealed) Class 로 사용! */
   Future<List<Note>> call(NoteOrder noteOrder) async {
     List<Note> notes = await repository.getNotes();
 
     noteOrder.when(
-      title: (orderType) {},
+      title: (orderType) {
+        orderType.when(
+          ascending: () {
+            notes.sort((a, b) => a.title.compareTo(b.title));
+          },
+          descending: () {
+            notes.sort((a, b) => -a.title.compareTo(b.title));
+          },
+        );
+      },
       date: (orderType) {
         orderType.when(
-          ascending: () {},
+          ascending: () {
+            notes.sort((a, b) => a.timestamp.compareTo(b.timestamp));
+          },
           descending: () {
             notes.sort((a, b) => -a.timestamp.compareTo(b.timestamp));
           },
         );
       },
-      color: (orderType) {},
+      color: (orderType) {
+        orderType.when(
+          ascending: () {
+            notes.sort((a, b) => a.color.compareTo(b.color));
+          },
+          descending: () {
+            notes.sort((a, b) => -a.color.compareTo(b.color));
+          },
+        );
+      },
     );
     return notes;
   }
